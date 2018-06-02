2a0d3f26fefe8819e0ead769078f0ad17336fa4f
Amar Singh Kaithwar <akaithwar@mdsol.com>
2018-06-02 16:26:48 +0530

    Added slug

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,18 @@ class Article < ApplicationRecord
  mount_uploader :image, ImageUploader
  validates_processing_of :image
  validate :image_size_validation
+ before_create :set_slug
+
+ def to_param
+    "#{id}-#{slug}"
+  end
 
  private
   def image_size_validation
     errors[:image] << "should be less than 500KB" if image.size > 0.5.megabytes
+  end
+
+  def set_slug
+    self.slug = title.parameterize
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_221211) do
+ActiveRecord::Schema.define(version: 2018_06_02_105044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_05_26_221211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.string "slug"
   end
 
 end
